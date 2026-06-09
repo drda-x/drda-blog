a405149a19761b73a62f1cf299c2faa73befd9d3
Tsouda <1239816490@qq.com>
2026-06-09 16:28:16 +0800

    Site updated: 2026-06-09 16:28:15

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1780993617761";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1780993693012";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
