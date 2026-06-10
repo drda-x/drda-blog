90f6224a2dfdf3f4134e1b3b88b73ff4f9ddcf48
Tsouda <1239816490@qq.com>
2026-06-10 13:38:53 +0800

    Site updated: 2026-06-10 13:38:50

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1781061828521";const preCache = ["/drda-blog/images/taichi.png","/drda-blog/images/banner.webp","/drda-blog/css/loader.css","/drda-blog/css/style.css","/drda-blog/js/script.js"];const cacheDomain = [
+const VERSION = "1781069925744";const preCache = ["/drda-blog/images/taichi.png","/drda-blog/images/banner.webp","/drda-blog/css/loader.css","/drda-blog/css/style.css","/drda-blog/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
