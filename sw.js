c66433d8a7274cc22b53bcb74c844897f254d04f
Tsouda <1239816490@qq.com>
2026-06-26 14:35:51 +0800

    Site updated: 2026-06-26 14:35:45

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1782455482865";const preCache = ["/drda-blog/images/taichi.png","/drda-blog/images/banner.webp","/drda-blog/css/loader.css","/drda-blog/css/style.css","/drda-blog/js/script.js"];const cacheDomain = [
+const VERSION = "1782455737725";const preCache = ["/drda-blog/images/taichi.png","/drda-blog/images/banner.webp","/drda-blog/css/loader.css","/drda-blog/css/style.css","/drda-blog/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
